fcb92436ab97bbf39b2c53d5c206ca4e524026d3
ValeriaGongar <valeria.gongar@gmail.com>
2022-08-23 17:37:56 -0500

    create layout- error doble child

---
+++ components/Layout.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - MyStore' : 'MyStore'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">MyStore</a>
+            </Link>
+            <div>
+              <Link href="/cart">Cart</Link>
+              <Link href="/login">Login</Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow inner">
+          <p>Creative Commons &copy; 2022 </p>
+        </footer>
+      </div>
+    </>
+  );
+}
